72108cb418f564baf11e16ea6ffeb83e4c229bdf
Bethan Harris <blharris24@gmail.com>
2019-06-04 17:35:25 +0100

    Nice plots for AE perturbations, in terms of (M, eta) and (r, z)

--- src/plot_vortex.py
+++ src/plot_vortex.py
@@ -22,7 +22,7 @@ def plot_variable(function):
     plt.show()
 
 
-def plot_available_energy_perturbations(r, z):
+def plot_available_energy_perturbations(r, z, show=True):
     base_M = angular_momentum(r, z)
     base_entropy = entropy(r, z)
 
@@ -30,12 +30,38 @@ def plot_available_energy_perturbations(r, z):
     all_M = angular_momentum(r_grid, z_grid)
     all_entropy = entropy(r_grid, z_grid)
     M_grid, entropy_grid = np.meshgrid(np.linspace(all_M.min(), all_M.max(), 100), np.linspace(all_entropy.min(), all_entropy.max(), 100))
-    ae = available_energy(M_grid, entropy_grid, r, z)
+    ae_M_eta = available_energy(M_grid, entropy_grid, r, z)
+    ae_r_z = available_energy(angular_momentum(r_grid, z_grid), entropy(r_grid, z_grid), r, z)
 
     plt.figure()
-    plt.contourf(M_grid-base_M, entropy_grid-base_entropy, ae, 20, cmap=cm.gist_heat_r)
-    plt.colorbar()
-    plt.show()
+    plt.contourf((M_grid-base_M)/1.e6, entropy_grid-base_entropy, ae_M_eta, 20, cmap=cm.gist_heat_r)
+    plt.xlabel(r'$\mathregular{M - M_0\;\left(10^6\,m^2s^{-1}\right)}$', fontsize=18)
+    plt.ylabel(r'$\mathregular{\eta - \eta_0\;\left(Jkg^{-1}K^{-1}\right)}$', fontsize=18)
+    plt.gca().tick_params(labelsize=14)
+    cbar = plt.colorbar()
+    cbar.set_label(r'$\mathregular{\Pi\;\left(Jkg^{-1}\right)}$', fontsize=18)
+    cbar.ax.tick_params(labelsize=14)
+    plt.title(r'$\mathregular{r_0 = %d \,km,\; z_0 = %d\, km}$' % (r/1000., z/1000.), fontsize=16)
+    plt.tight_layout()
+    plt.savefig('../results/ae_perturbation_M_eta_r_%d_z_%d.png' % (r, z), dpi=300)
+
+    plt.figure()
+    plt.contourf((r_grid-r)/1000., (z_grid-z)/1000., ae_r_z, 20, cmap=cm.gist_heat_r)
+    plt.xlabel(r'$\mathregular{r - r_0\;\left(km\right)}$', fontsize=18)
+    plt.ylabel(r'$\mathregular{z - z_0\;\left(km\right)}$', fontsize=18)
+    plt.gca().tick_params(labelsize=14)
+    cbar = plt.colorbar()
+    cbar.set_label(r'$\mathregular{\Pi\;\left(Jkg^{-1}\right)}$', fontsize=18)
+    cbar.ax.tick_params(labelsize=14)
+    plt.title(r'$\mathregular{r_0 = %d \,km,\; z_0 = %d\, km}$' % (r / 1000., z / 1000.), fontsize=16)
+    plt.tight_layout()
+    plt.savefig('../results/ae_perturbation_rz_r_%d_z_%d.png' % (r, z), dpi=300)
+
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
     return M_grid-base_M, entropy_grid-base_entropy, ae
 
 
