ad70b0269e3b51317b0070fe8b61390e2f881fea
Bethan Harris <blharris24@gmail.com>
2019-06-04 15:51:25 +0100

    Remove old functions

--- src/plot_vortex.py
+++ src/plot_vortex.py
@@ -22,24 +22,6 @@ def plot_variable(function):
     plt.show()
 
 
-def plot_available_energy(r_ref, z_ref):
-    r_list = np.linspace(r_ref - 10000., r_ref + 10000., 501, endpoint=True)
-    z_list = np.linspace(z_ref - 1000., z_ref + 1000., 501, endpoint=True)
-    r_grid, z_grid = np.meshgrid(r_list, z_list)
-    M_ref = angular_momentum(r_ref, z_ref)
-    M_grid = angular_momentum(r_grid, z_grid)
-    entropy_ref = entropy(r_ref, z_ref)
-    entropy_grid = entropy(r_grid, z_grid)
-    ae = available_energy(r_grid, z_grid, r_ref, z_ref)
-    plt.figure()
-    plt.contourf(r_grid-r_ref, z_grid-z_ref, ae, 100, cmap=cm.seismic)
-    plt.colorbar()
-    plt.figure()
-    plt.contourf(entropy_grid - entropy_ref, M_grid - M_ref, ae, 100, cmap=cm.seismic)
-    plt.colorbar()
-    plt.show()
-
-
 def plot_available_energy_perturbations(r, z):
     base_M = angular_momentum(r, z)
     base_entropy = entropy(r, z)
@@ -47,7 +29,7 @@ def plot_available_energy_perturbations(r, z):
     r_grid, z_grid = r_z_grid()
     all_M = angular_momentum(r_grid, z_grid)
     all_entropy = entropy(r_grid, z_grid)
-    M_grid, entropy_grid = np.meshgrid(np.linspace(all_M.min(), all_M.max(), 100.), np.linspace(all_entropy.min(), all_entropy.max(), 100.))
+    M_grid, entropy_grid = np.meshgrid(np.linspace(all_M.min(), all_M.max(), 100), np.linspace(all_entropy.min(), all_entropy.max(), 100))
     ae = available_energy(M_grid, entropy_grid, r, z)
 
     plt.figure()
