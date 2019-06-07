647afb86a679270a45538f73e85c6c05a90e8964
gt890397 <b.l.harris@pgr.reading.ac.uk>
2019-06-07 15:18:49 +0100

    Allow perturbations of pressure, calculate Pi1 and Pi2 separately.

--- src/available_energy.py
+++ src/available_energy.py
@@ -88,14 +88,29 @@ def reference_position(angular_momentum_parcel, entropy_parcel):
     return r_conserving_angular_momentum, mid_z_new
 
 
-def available_energy(M, eta, r, z):
+def available_elastic_energy(eta, p, r, z):
+    temperature_in_situ = temperature_from_entropy(eta, p)
+    density_in_situ = p/(Rd * temperature_in_situ)
+    enthalpy_in_situ = cp * temperature_in_situ
+    enthalpy_at_pref = cp * lifted_temperature(temperature_in_situ, p, pressure(r, z))
+    pressure_head = (pressure(r, z) - p)/density_in_situ
+    return enthalpy_in_situ - enthalpy_at_pref + pressure_head
+
+
+def available_potential_energy(M, eta, r, z):
     r_ref, z_ref = reference_position(M, eta)
     M_terms = 0.5 * M**2 * (1. / r ** 2 - 1. / r_ref ** 2) + 0.125 * f ** 2 * (r ** 2 - r_ref ** 2)
     geopotential_terms = geopotential(z) - geopotential(z_ref)
-    temperature_in_situ = temperature_from_entropy(eta, pressure(r, z))
-    enthalpy_in_situ = cp * temperature_in_situ
-    enthalpy_at_reference = cp * lifted_temperature(temperature_in_situ, pressure(r, z), pressure(r_ref, z_ref))
-    return M_terms + geopotential_terms + enthalpy_in_situ - enthalpy_at_reference
+    temperature_at_p0 = temperature_from_entropy(eta, pressure(r, z))
+    enthalpy_at_p0 = cp * temperature_at_p0
+    enthalpy_at_reference = cp * lifted_temperature(temperature_at_p0, pressure(r, z), pressure(r_ref, z_ref))
+    return M_terms + geopotential_terms + enthalpy_at_p0 - enthalpy_at_reference
+
+
+def available_energy(M, eta, p, r, z):
+    aee = available_elastic_energy(eta, p, r, z)
+    ape = available_potential_energy(M, eta, r, z)
+    return aee + ape
 
 
 def available_energy_perturbations_M_eta(r, z):
@@ -105,8 +120,30 @@ def available_energy_perturbations_M_eta(r, z):
     all_M = angular_momentum(r_grid, z_grid)
     all_entropy = entropy(r_grid, z_grid)
     M_grid, entropy_grid = np.meshgrid(np.linspace(all_M.min(), all_M.max(), 100), np.linspace(all_entropy.min(), all_entropy.max(), 100))
-    ae_M_eta = available_energy(M_grid, entropy_grid, r, z)
+    ae_M_eta = available_energy(M_grid, entropy_grid, pressure(r, z), r, z)
     return M_grid-base_M, entropy_grid-base_entropy, ae_M_eta
+
+
+def available_energy_perturbations_M_pressure(r, z):
+    base_M = angular_momentum(r, z)
+    base_pressure = pressure(r, z)
+    r_grid, z_grid = r_z_grid()
+    all_M = angular_momentum(r_grid, z_grid)
+    all_pressure = pressure(r_grid, z_grid)
+    M_grid, pressure_grid = np.meshgrid(np.linspace(all_M.min(), all_M.max(), 100), np.linspace(all_pressure.min(), all_pressure.max(), 100))
+    ae_M_pressure = available_energy(M_grid, np.ones_like(M_grid)*entropy(r, z), pressure_grid, r, z)
+    return M_grid-base_M, pressure_grid-base_pressure, ae_M_pressure
+
+
+def available_energy_perturbations_eta_pressure(r, z):
+    base_eta = entropy(r, z)
+    base_pressure = pressure(r, z)
+    r_grid, z_grid = r_z_grid()
+    all_eta = entropy(r_grid, z_grid)
+    all_pressure = pressure(r_grid, z_grid)
+    eta_grid, pressure_grid = np.meshgrid(np.linspace(all_eta.min(), all_eta.max(), 100), np.linspace(all_pressure.min(), all_pressure.max(), 100))
+    ae_eta_pressure = available_energy(np.ones_like(eta_grid)*angular_momentum(r, z), eta_grid, pressure_grid, r, z)
+    return eta_grid-base_eta, pressure_grid-base_pressure, ae_eta_pressure
 
 
 def available_energy_perturbations_r_z(r, z):
