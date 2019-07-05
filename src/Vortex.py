e08bb7991db2981e5d82b1b9b021d5688d5f0d7d
gt890397 <b.l.harris@pgr.reading.ac.uk>
2019-07-05 17:26:03 +0100

    Add methods for mu and chi

--- src/Vortex.py
+++ src/Vortex.py
@@ -16,7 +16,6 @@ import numpy as np
 
 
 class Vortex:
-
     f = 6.14e-5
     g = 9.81
     Rd = 287.
@@ -31,61 +30,70 @@ class Vortex:
         self.surface_temperature = t_surf
         self.lapse_rate = gamma
         self.central_pressure = p_centre
-        self.pressure_deficit = self.central_pressure -self.far_field_pressure
+        self.pressure_deficit = self.central_pressure - self.far_field_pressure
         self.radial_scale = rs
         self.vertical_scale = zs
 
     def grid(self, return_lists=False):
-        r_list = np.linspace(1., self.radial_edge, int(self.radial_edge/1000. + 1.), endpoint=True)
-        z_list = np.linspace(1., self.vertical_top, int(self.vertical_top/1000. + 1.), endpoint=True)
+        r_list = np.linspace(1., self.radial_edge, int(self.radial_edge / 1000. + 1.), endpoint=True)
+        z_list = np.linspace(1., self.vertical_top, int(self.vertical_top / 1000. + 1.), endpoint=True)
         r_grid, z_grid = np.meshgrid(r_list, z_list)
         if return_lists:
             return r_list, z_list
         else:
             return r_grid, z_grid
 
+    def gridded_variable(self, variable):
+        r_grid, z_grid = self.grid()
+        return variable(r_grid, z_grid)
+
     def pressure_perturbation(self, r, z):
-        radial_term = 1. - np.exp(-self.radial_coefficient*self.radial_scale/r)
-        vertical_term = np.exp(-z/self.vertical_scale)*np.cos(0.5*np.pi*z/self.vertical_top)
-        return self.pressure_deficit*radial_term*vertical_term
+        radial_term = 1. - np.exp(-self.radial_coefficient * self.radial_scale / r)
+        vertical_term = np.exp(-z / self.vertical_scale) * np.cos(0.5 * np.pi * z / self.vertical_top)
+        return self.pressure_deficit * radial_term * vertical_term
 
     def environment_temperature(self, z):
-        return self.surface_temperature * (1. - self.lapse_rate*z)
+        return self.surface_temperature * (1. - self.lapse_rate * z)
 
     def environment_pressure(self, z):
-        return self.far_field_pressure * np.exp((self.g*np.log(1. - self.lapse_rate*z))/(self.Rd*self.surface_temperature*self.lapse_rate))
+        return self.far_field_pressure * np.exp(
+            (self.g * np.log(1. - self.lapse_rate * z)) / (self.Rd * self.surface_temperature * self.lapse_rate))
 
     def environment_pressure_vertical_gradient(self, z):
-        return -(self.far_field_pressure*self.g*(1. - self.lapse_rate*z)**(self.g/(self.Rd*self.surface_temperature*self.lapse_rate) - 1.))/(self.Rd*self.surface_temperature)
+        return -(self.far_field_pressure * self.g * (1. - self.lapse_rate * z) ** (
+                    self.g / (self.Rd * self.surface_temperature * self.lapse_rate) - 1.)) / (
+                           self.Rd * self.surface_temperature)
 
     def pressure(self, r, z):
         return self.environment_pressure(z) + self.pressure_perturbation(r, z)
 
     def pressure_vertical_gradient(self, r, z):
-        radial_term = 1. - np.exp(-self.radial_coefficient*self.radial_scale/r)
+        radial_term = 1. - np.exp(-self.radial_coefficient * self.radial_scale / r)
         vertical_term = -np.exp(-z / self.vertical_scale) * (1. / self.vertical_scale) * (
-                    (0.5 * np.pi * self.vertical_scale / self.vertical_top) * np.sin(0.5 * np.pi * z / self.vertical_top) + np.cos(
-                0.5 * np.pi * z / self.vertical_top))
-        return self.pressure_deficit*radial_term*vertical_term + self.environment_pressure_vertical_gradient(z)
+                (0.5 * np.pi * self.vertical_scale / self.vertical_top) * np.sin(
+            0.5 * np.pi * z / self.vertical_top) + np.cos(
+            0.5 * np.pi * z / self.vertical_top))
+        return self.pressure_deficit * radial_term * vertical_term + self.environment_pressure_vertical_gradient(z)
 
     def pressure_radial_gradient(self, r, z):
-        radial_term = -self.radial_coefficient*self.radial_scale*np.exp(-self.radial_coefficient*self.radial_scale/r)/(r**2)
+        radial_term = -self.radial_coefficient * self.radial_scale * np.exp(
+            -self.radial_coefficient * self.radial_scale / r) / (r ** 2)
         vertical_term = np.exp(-z / self.vertical_scale) * np.cos(0.5 * np.pi * z / self.vertical_top)
         return self.pressure_deficit * radial_term * vertical_term
 
     def density(self, r, z):
-        return -self.pressure_vertical_gradient(r, z)/self.g
+        return -self.pressure_vertical_gradient(r, z) / self.g
 
     def temperature(self, r, z):
-        return self.pressure(r, z)/(self.Rd * self.density(r, z))
+        return self.pressure(r, z) / (self.Rd * self.density(r, z))
 
     def gradient_wind_term(self, r, z):
-        #fv + v^2/r
-        return self.pressure_radial_gradient(r, z)/self.density(r, z)
+        # fv + v^2/r
+        return self.pressure_radial_gradient(r, z) / self.density(r, z)
 
     def azimuthal_wind(self, r, z):
         gradient_wind = self.gradient_wind_term(r, z)
-        v = 0.5*r*(-self.f + np.sqrt(self.f**2 + 4*gradient_wind/r))
+        v = 0.5 * r * (-self.f + np.sqrt(self.f ** 2 + 4 * gradient_wind / r))
         return v
 
     def angular_momentum(self, r, z):
@@ -99,3 +107,18 @@ class Vortex:
 
     def geopotential(self, z):
         return self.g * z
+
+    def mu(self, r, z):
+        return self.angular_momentum(r, z)**2
+
+    @staticmethod
+    def chi(r):
+        return 1./(2. * r**2)
+
+
+def smith_vortex():
+    return Vortex(303., 2.12e-5, 95000., 50000., 8000.)
+
+
+def perturbed_vortex():
+    return Vortex(305., 2.12e-5, 93000., 30000., 8000.)
