f7e1660416994a6d161b2dd0e894784ec1961671
gt890397 <b.l.harris@pgr.reading.ac.uk>
2019-07-05 17:25:27 +0100

    Add perturbation plots for mu/p, illustration of lifting procedure

--- src/available_energy.py
+++ src/available_energy.py
@@ -1,28 +1,30 @@
-from derived_variables import *
-from utils import r_z_grid
+import numpy as np
+from Vortex import Vortex
 
 
-def lifted_temperature(old_temperature, old_pressure, new_pressure):
-    new_temperature = old_temperature * (new_pressure / old_pressure) ** (Rd / cp)
+def lifted_temperature(vortex, old_temperature, old_pressure, new_pressure):
+    new_temperature = old_temperature * (new_pressure / old_pressure) ** (vortex.Rd / vortex.cp)
     return new_temperature
 
 
-def temperature_from_entropy(entropy, pressure):
-    potential_temperature = np.exp(entropy/cp)
-    temperature = potential_temperature*(pressure/p0)**(Rd/cp)
+def temperature_from_entropy(vortex, entropy, pressure):
+    potential_temperature = np.exp(entropy/vortex.cp)
+    temperature = potential_temperature*(pressure/vortex.p0)**(vortex.Rd/vortex.cp)
     return temperature
 
-def potential_temperature_from_entropy(entropy):
-    potential_temperature = np.exp(entropy/cp)
+
+def potential_temperature_from_entropy(vortex, entropy):
+    potential_temperature = np.exp(entropy/vortex.cp)
     return potential_temperature
 
-def entropy_from_temperature(temperature, pressure):
-    potential_temperature = temperature*(p0/pressure)**(Rd/cp)
-    entropy = cp*np.log(potential_temperature)
+
+def entropy_from_temperature(vortex, temperature, pressure):
+    potential_temperature = temperature*(vortex.p0/pressure)**(vortex.Rd/vortex.cp)
+    entropy = vortex.cp*np.log(potential_temperature)
     return entropy
 
 
-def angular_momentum_radius_from_height(angular_momentum_parcel, z):
+def angular_momentum_radius_from_height(vortex, angular_momentum_parcel, z):
     if isinstance(angular_momentum_parcel, np.float32) or isinstance(angular_momentum_parcel, np.float64) or isinstance(angular_momentum_parcel, float):
         z = np.array([z])
         angular_momentum_parcel = np.array([angular_momentum_parcel])
@@ -41,7 +43,7 @@ def angular_momentum_radius_from_height(angular_momentum_parcel, z):
     # Bisect
     for _ in np.arange(0, steps):
         mid_r = (low_r + high_r) / 2.
-        angular_momentum_mid = angular_momentum(mid_r, z)
+        angular_momentum_mid = vortex.angular_momentum(mid_r, z)
         delta_angular_momentum_mid = angular_momentum_mid - angular_momentum_parcel
         pos_angular_momentum = np.where(delta_angular_momentum_mid > 0.)
         neg_angular_momentum = np.where(delta_angular_momentum_mid < 0.)
@@ -53,7 +55,7 @@ def angular_momentum_radius_from_height(angular_momentum_parcel, z):
     return mid_r_new
 
 
-def reference_position(angular_momentum_parcel, entropy_parcel):
+def reference_position(vortex, angular_momentum_parcel, entropy_parcel):
     if isinstance(entropy_parcel, np.float32) or isinstance(entropy_parcel, np.float64) or isinstance(entropy_parcel, float):
         entropy_parcel = np.array([entropy_parcel])
         angular_momentum_parcel = np.array([angular_momentum_parcel])
@@ -72,8 +74,8 @@ def reference_position(angular_momentum_parcel, entropy_parcel):
     # Bisect
     for _ in np.arange(0, steps):
         mid_z = (low_z + high_z) / 2.
-        r_conserving_angular_momentum = angular_momentum_radius_from_height(angular_momentum_parcel, mid_z)
-        entropy_mid = entropy(r_conserving_angular_momentum, mid_z)
+        r_conserving_angular_momentum = angular_momentum_radius_from_height(vortex, angular_momentum_parcel, mid_z)
+        entropy_mid = vortex.entropy(r_conserving_angular_momentum, mid_z)
         delta_entropy_mid = entropy_mid - entropy_parcel
         pos_entropy = np.where(delta_entropy_mid > 0.)
         neg_entropy = np.where(delta_entropy_mid < 0.)
@@ -83,70 +85,113 @@ def reference_position(angular_momentum_parcel, entropy_parcel):
 
         mid_z_new = (low_z + high_z) / 2.
 
-    r_conserving_angular_momentum = angular_momentum_radius_from_height(angular_momentum_parcel, mid_z_new)
+    r_conserving_angular_momentum = angular_momentum_radius_from_height(vortex, angular_momentum_parcel, mid_z_new)
 
     return r_conserving_angular_momentum, mid_z_new
 
 
-def available_elastic_energy(eta, p, r, z):
-    temperature_in_situ = temperature_from_entropy(eta, p)
-    density_in_situ = p/(Rd * temperature_in_situ)
-    enthalpy_in_situ = cp * temperature_in_situ
-    enthalpy_at_pref = cp * lifted_temperature(temperature_in_situ, p, pressure(r, z))
-    pressure_head = (pressure(r, z) - p)/density_in_situ
+def position_at_isobaric_surface(vortex, angular_momentum_parcel, reference_pressure_parcel):
+    if isinstance(reference_pressure_parcel, np.float32) or isinstance(reference_pressure_parcel, np.float64) or isinstance(reference_pressure_parcel, float):
+        reference_pressure_parcel = np.array([reference_pressure_parcel])
+        angular_momentum_parcel = np.array([angular_momentum_parcel])
+
+    dt_bound = 1e-10  # desired precision of final position
+
+    lower_bound_z = 0.
+    upper_bound_z = 16000.
+
+    low_z = np.full(angular_momentum_parcel.shape, lower_bound_z)
+    high_z = np.full(angular_momentum_parcel.shape, upper_bound_z)
+
+    # Find number of bisection steps required to reach desired accuracy
+    steps = np.ceil(np.log2((upper_bound_z - lower_bound_z) / dt_bound))
+
+    # Bisect
+    for _ in np.arange(0, steps):
+        mid_z = (low_z + high_z) / 2.
+        r_conserving_angular_momentum = angular_momentum_radius_from_height(vortex, angular_momentum_parcel, mid_z)
+        pressure_mid = vortex.pressure(r_conserving_angular_momentum, mid_z)
+        delta_pressure_mid = pressure_mid - reference_pressure_parcel
+        pos_pressure = np.where(delta_pressure_mid > 0.)
+        neg_pressure = np.where(delta_pressure_mid < 0.)
+
+        low_z[pos_pressure] = mid_z[pos_pressure]
+        high_z[neg_pressure] = mid_z[neg_pressure]
+
+        mid_z_new = (low_z + high_z) / 2.
+
+    r_conserving_angular_momentum = angular_momentum_radius_from_height(vortex, angular_momentum_parcel, mid_z_new)
+
+    return r_conserving_angular_momentum, mid_z_new
+
+
+def available_elastic_energy(vortex, entropy, p, r, z):
+    temperature_in_situ = temperature_from_entropy(vortex, entropy, p)
+    density_in_situ = p/(vortex.Rd * temperature_in_situ)
+    enthalpy_in_situ = vortex.cp * temperature_in_situ
+    enthalpy_at_pref = vortex.cp * lifted_temperature(vortex, temperature_in_situ, p, vortex.pressure(r, z))
+    pressure_head = (vortex.pressure(r, z) - p)/density_in_situ
     return enthalpy_in_situ - enthalpy_at_pref + pressure_head
 
 
-def available_potential_energy(M, eta, r, z):
-    r_ref, z_ref = reference_position(M, eta)
-    M_terms = 0.5 * M**2 * (1. / r ** 2 - 1. / r_ref ** 2) + 0.125 * f ** 2 * (r ** 2 - r_ref ** 2)
-    geopotential_terms = geopotential(z) - geopotential(z_ref)
-    temperature_at_p0 = temperature_from_entropy(eta, pressure(r, z))
-    enthalpy_at_p0 = cp * temperature_at_p0
-    enthalpy_at_reference = cp * lifted_temperature(temperature_at_p0, pressure(r, z), pressure(r_ref, z_ref))
+def available_potential_energy(vortex, M, eta, r, z):
+    r_ref, z_ref = reference_position(vortex, M, eta)
+    M_terms = 0.5 * M**2 * (1. / r ** 2 - 1. / r_ref ** 2) + 0.125 * vortex.f ** 2 * (r ** 2 - r_ref ** 2)
+    geopotential_terms = vortex.geopotential(z) - vortex.geopotential(z_ref)
+    temperature_at_p0 = temperature_from_entropy(vortex, eta, vortex.pressure(r, z))
+    enthalpy_at_p0 = vortex.cp * temperature_at_p0
+    enthalpy_at_reference = vortex.cp * lifted_temperature(vortex, temperature_at_p0, vortex.pressure(r, z), vortex.pressure(r_ref, z_ref))
     return M_terms + geopotential_terms + enthalpy_at_p0 - enthalpy_at_reference
 
 
-def available_energy(M, eta, p, r, z):
-    aee = available_elastic_energy(eta, p, r, z)
-    ape = available_potential_energy(M, eta, r, z)
+def pi_e(vortex, M, eta, r, z):
+    r_mu, z_mu = position_at_isobaric_surface(vortex, M, vortex.pressure(r, z))
+    r_ref, z_ref = reference_position(vortex, M, eta)
+    M_terms = 0.5 * M**2 * (1. / r_mu ** 2 - 1. / r_ref ** 2) + 0.125 * vortex.f ** 2 * (r_mu ** 2 - r_ref ** 2)
+    geopotential_terms = vortex.geopotential(z_mu) - vortex.geopotential(z_ref)
+    temperature_at_p0 = temperature_from_entropy(vortex, eta, vortex.pressure(r, z))
+    enthalpy_at_p0 = vortex.cp * temperature_at_p0
+    enthalpy_at_reference = vortex.cp * lifted_temperature(vortex, temperature_at_p0, vortex.pressure(r, z), vortex.pressure(r_ref, z_ref))
+    return M_terms + geopotential_terms + enthalpy_at_p0 - enthalpy_at_reference
+
+
+def pi_k(vortex, M, r, z):
+    r_mu, z_mu = position_at_isobaric_surface(vortex, M, vortex.pressure(r, z))
+    M_terms = 0.5 * M**2 * (1. / r ** 2 - 1. / r_mu ** 2) + 0.125 * vortex.f ** 2 * (r ** 2 - r_mu ** 2)
+    geopotential_terms = vortex.geopotential(z) - vortex.geopotential(z_mu)
+    return M_terms + geopotential_terms
+
+
+def available_energy(vortex, M, eta, p, r, z):
+    aee = available_elastic_energy(vortex, eta, p, r, z)
+    ape = available_potential_energy(vortex, M, eta, r, z)
     return aee + ape
 
 
-def available_energy_perturbations_M_eta(r, z):
-    base_M = angular_momentum(r, z)
-    base_entropy = entropy(r, z)
-    r_grid, z_grid = r_z_grid()
-    all_M = angular_momentum(r_grid, z_grid)
-    all_entropy = entropy(r_grid, z_grid)
+def available_potential_energy_perturbations_M_entropy(vortex, r, z):
+    base_M = vortex.angular_momentum(r, z)
+    base_entropy = vortex.entropy(r, z)
+    all_M = vortex.gridded_variable(vortex.angular_momentum)
+    all_entropy = vortex.gridded_variable(vortex.entropy)
     M_grid, entropy_grid = np.meshgrid(np.linspace(all_M.min(), all_M.max(), 100), np.linspace(all_entropy.min(), all_entropy.max(), 100))
-    ae_M_eta = available_energy(M_grid, entropy_grid, pressure(r, z), r, z)
+    ae_M_eta = available_potential_energy(vortex, M_grid, entropy_grid, r, z)
     return M_grid-base_M, entropy_grid-base_entropy, ae_M_eta
 
 
-def available_energy_perturbations_M_pressure(r, z):
-    base_M = angular_momentum(r, z)
-    base_pressure = pressure(r, z)
-    r_grid, z_grid = r_z_grid()
-    all_M = angular_momentum(r_grid, z_grid)
-    all_pressure = pressure(r_grid, z_grid)
-    M_grid, pressure_grid = np.meshgrid(np.linspace(all_M.min(), all_M.max(), 100), np.linspace(all_pressure.min(), all_pressure.max(), 100))
-    ae_M_pressure = available_energy(M_grid, np.ones_like(M_grid)*entropy(r, z), pressure_grid, r, z)
-    return M_grid-base_M, pressure_grid-base_pressure, ae_M_pressure
+def available_potential_energy_perturbations_mu_pressure(vortex, r, z):
+    base_mu = vortex.mu(r, z)
+    base_pressure = vortex.pressure(r, z)
+    all_M = vortex.gridded_variable(vortex.angular_momentum)
+    all_entropy = vortex.gridded_variable(vortex.entropy)
+    M_grid, entropy_grid = np.meshgrid(np.linspace(all_M.min(), all_M.max(), 100), np.linspace(all_entropy.min(), all_entropy.max(), 100))
+    mu_grid = M_grid**2
+    r_ref, z_ref = reference_position(vortex, M_grid, entropy_grid)
+    p_ref_grid = vortex.pressure(r_ref, z_ref)
+    ae_M_eta = available_potential_energy(vortex, M_grid, entropy_grid, r, z)
+    return base_mu-mu_grid, base_pressure-p_ref_grid, ae_M_eta
 
 
-def available_energy_perturbations_eta_pressure(r, z):
-    base_eta = entropy(r, z)
-    base_pressure = pressure(r, z)
-    r_grid, z_grid = r_z_grid()
-    all_eta = entropy(r_grid, z_grid)
-    all_pressure = pressure(r_grid, z_grid)
-    eta_grid, pressure_grid = np.meshgrid(np.linspace(all_eta.min(), all_eta.max(), 100), np.linspace(all_pressure.min(), all_pressure.max(), 100))
-    ae_eta_pressure = available_energy(np.ones_like(eta_grid)*angular_momentum(r, z), eta_grid, pressure_grid, r, z)
-    return eta_grid-base_eta, pressure_grid-base_pressure, ae_eta_pressure
-
-
-def available_energy_perturbations_r_z(r, z):
-    r_grid, z_grid = r_z_grid()
-    ae_r_z = available_energy(angular_momentum(r_grid, z_grid), entropy(r_grid, z_grid), r, z)
+def available_potential_energy_perturbations_r_z(vortex, r, z):
+    r_grid, z_grid = vortex.grid()
+    ae_r_z = available_potential_energy(vortex, vortex.gridded_variable(vortex.angular_momentum), vortex.gridded_variable(vortex.entropy), r, z)
     return r_grid-r, z_grid-z, ae_r_z
--- src/plot_vortex.py
+++ src/plot_vortex.py
@@ -1,55 +1,98 @@
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-from utils import grid_variable
 from available_energy import *
 
 
-def plot_variable(variable):
+def plot_variable(vortex, variable, label=''):
     plt.figure()
-    plt.contourf(grid_variable(variable), cmap=cm.YlGn)
-    plt.colorbar()
+    r_grid, z_grid = vortex.grid()
+    plt.contourf(r_grid/1000., z_grid/1000., vortex.gridded_variable(variable), cmap=cm.YlGn)
+    plt.xlabel(r'$\mathregular{r\;\left(km\right)}$', fontsize=18)
+    plt.ylabel(r'$\mathregular{z\;\left(km\right)}$', fontsize=18)
+    plt.gca().tick_params(labelsize=14)
+    cbar = plt.colorbar()
+    cbar.set_label(label, fontsize=18)
+    cbar.ax.tick_params(labelsize=14)
+    plt.tight_layout()
     plt.show()
 
 
-def plot_available_energy_perturbations(r, z, show=True):
-    pertubation_M, perturbation_eta, ae_M_eta = available_energy_perturbations_M_eta(r, z)
-    pertubation_r, perturbation_z, ae_r_z = available_energy_perturbations_r_z(r, z)
+def plot_available_energy_perturbations(vortex, r, z, save=False, show=True):
+    perturbation_M, perturbation_eta, ae_M_entropy = available_potential_energy_perturbations_M_entropy(vortex, r, z)
+    perturbation_mu, perturbation_p, ae_mu_p = available_potential_energy_perturbations_mu_pressure(vortex, r, z)
+    perturbation_r, perturbation_z, ae_r_z = available_potential_energy_perturbations_r_z(vortex, r, z)
 
     plt.figure()
-    plt.contourf((pertubation_M)/1.e6, perturbation_eta, ae_M_eta, 20, cmap=cm.gist_heat_r)
+    plt.contourf(perturbation_M/1.e6, perturbation_eta, ae_M_entropy, 20, cmap=cm.gist_heat_r)
     plt.xlabel(r'$\mathregular{M - M_0\;\left(10^6\,m^2s^{-1}\right)}$', fontsize=18)
     plt.ylabel(r'$\mathregular{\eta - \eta_0\;\left(Jkg^{-1}K^{-1}\right)}$', fontsize=18)
     plt.gca().tick_params(labelsize=14)
     cbar = plt.colorbar()
-    cbar.set_label(r'$\mathregular{\Pi\;\left(Jkg^{-1}\right)}$', fontsize=18)
+    cbar.set_label(r'$\mathregular{A_e\;\left(Jkg^{-1}\right)}$', fontsize=18)
     cbar.ax.tick_params(labelsize=14)
-    plt.title(r'$\mathregular{r_0 = %g \,km,\; z_0 = %g\, km}$' % (r/1000., z/1000.), fontsize=16)
+    plt.title(r'$\mathregular{r = %g \,km,\; z = %g\, km}$' % (r/1000., z/1000.), fontsize=16)
     plt.tight_layout()
-    plt.savefig('../results/ae_perturbation_M_eta_r_%d_z_%d.png' % (r, z), dpi=300)
+    if save:
+        plt.savefig('../results/ae_perturbation_M_eta_r_%d_z_%d.png' % (r, z), dpi=300)
 
     plt.figure()
-    plt.contourf((pertubation_r)/1000., (perturbation_z)/1000., ae_r_z, 20, cmap=cm.gist_heat_r)
-    plt.xlabel(r'$\mathregular{r - r_0\;\left(km\right)}$', fontsize=18)
-    plt.ylabel(r'$\mathregular{z - z_0\;\left(km\right)}$', fontsize=18)
+    plt.contourf(perturbation_mu/1.e6, perturbation_p, ae_mu_p, 20, cmap=cm.gist_heat_r)
+    plt.xlabel(r'$\mathregular{\mu - \mu_m\;\left(10^6\,m^4s^{-2}\right)}$', fontsize=18)
+    plt.ylabel(r'$\mathregular{p_m - p_\star\;\left(Jkg^{-1}K^{-1}\right)}$', fontsize=18)
     plt.gca().tick_params(labelsize=14)
     cbar = plt.colorbar()
-    cbar.set_label(r'$\mathregular{\Pi\;\left(Jkg^{-1}\right)}$', fontsize=18)
+    cbar.set_label(r'$\mathregular{A_e\;\left(Jkg^{-1}\right)}$', fontsize=18)
     cbar.ax.tick_params(labelsize=14)
-    plt.title(r'$\mathregular{r_0 = %g \,km,\; z_0 = %g\, km}$' % (r / 1000., z / 1000.), fontsize=16)
+    plt.title(r'$\mathregular{r = %g \,km,\; z = %g\, km}$' % (r/1000., z/1000.), fontsize=16)
     plt.tight_layout()
-    plt.savefig('../results/ae_perturbation_rz_r_%d_z_%d.png' % (r, z), dpi=300)
+    if save:
+        plt.savefig('../results/ae_perturbation_mu_p_r_%d_z_%d.png' % (r, z), dpi=300)
+
+    plt.figure()
+    plt.contourf(perturbation_r/1000., perturbation_z/1000., ae_r_z, 20, cmap=cm.gist_heat_r)
+    plt.xlabel(r'$\mathregular{r_\star - r\;\left(km\right)}$', fontsize=18)
+    plt.ylabel(r'$\mathregular{z_\star - z\;\left(km\right)}$', fontsize=18)
+    plt.gca().tick_params(labelsize=14)
+    cbar = plt.colorbar()
+    cbar.set_label(r'$\mathregular{A_e\;\left(Jkg^{-1}\right)}$', fontsize=18)
+    cbar.ax.tick_params(labelsize=14)
+    plt.title(r'$\mathregular{r = %g \,km,\; z = %g\, km}$' % (r / 1000., z / 1000.), fontsize=16)
+    plt.tight_layout()
+    if save:
+        plt.savefig('../results/ae_perturbation_rzref_r_%d_z_%d.png' % (r, z), dpi=300)
 
     if show:
         plt.show()
     else:
-        plt.close()
+        plt.close('all')
 
     return
 
 
+def illustrate_lifting(vortex):
+    r = 50000.
+    z = 10000.
+    M = vortex.angular_momentum(30000., 3000.)
+    eta = vortex.entropy(30000., 3000.)
+    r_grid, z_grid = vortex.grid()
+    r_mu, z_mu = position_at_isobaric_surface(vortex, M, vortex.pressure(r, z))
+    r_ref, z_ref = reference_position(vortex, M, eta)
+    plt.contour(r_grid / 1000., z_grid / 1000., vortex.gridded_variable(vortex.pressure), colors='k', linestyles='--')
+    plt.contour(r_grid / 1000., z_grid / 1000., vortex.gridded_variable(vortex.entropy), colors='k', linestyles='-')
+    plt.contour(r_grid / 1000., z_grid / 1000., vortex.gridded_variable(vortex.angular_momentum), colors='k',
+                linestyles=':')
+    plt.plot(r/1000., z/1000., 'ro')
+    plt.annotate('$\left(r, z\\right)$', xy=(r/1000., z/1000.), xytext=(5, 5), textcoords='offset points')
+    plt.plot(r_mu/1000., z_mu/1000., 'go')
+    plt.annotate('$\left(r_{\mu}, z_{\mu}\\right)$', xy=(r_mu / 1000., z_mu / 1000.), xytext=(-40, 5), textcoords='offset points')
+    plt.plot(r_ref/1000., z_ref/1000., 'bo')
+    plt.annotate('$\left(r_{\star}, z_{\star}\\right)$', xy=(r_ref / 1000., z_ref / 1000.), xytext=(-10, -15), textcoords='offset points')
+    plt.tight_layout()
+
+
 if __name__ == '__main__':
-    r0_list = [100., 1000., 2500., 5000., 10000., 25000., 40000., 50000., 100000., 150000., 200000.]
-    z0_list = [10., 100., 250., 500., 1000., 2500., 4000., 5000., 10000., 15000.]
-    for r0 in r0_list:
-        for z0 in z0_list:
-            plot_available_energy_perturbations(r0, z0, show=False)
+    r_list = [2500., 5000., 10000., 20000., 25000., 50000., 75000., 100000., 150000.]
+    z_list = [0., 1000., 2000., 5000., 8000., 10000., 12000., 15000.]
+    for r in r_list:
+        for z in z_list:
+            plot_available_energy_perturbations(r, z, show=False)
